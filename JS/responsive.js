4b27d535f4cc2e1f7b5966b6c36af7425add4f46
vicious721 <du-du7@hotmail.com>
2018-06-01 03:50:12 -0300

    Tentando fazer um js sem saber js

--- JS/responsive.js
+++ JS/responsive.js
@@ -10,7 +10,6 @@ function openNav() {
     if (aberto === false){
         document.getElementById("Sidenav_id").style.width = "250px";
         aberto = true;
-        console.log(aberto);
     } else {
         closeNav();
     }
@@ -19,4 +18,39 @@ function openNav() {
 function closeNav() {
     document.getElementById("Sidenav_id").style.width = "0";
     aberto = false;
+}
+
+
+window.onload = function(){ 
+    var votacao = document.getElementById("votacao_id");
+    votacao.onclick = function(event) {
+            if (typeof(Storage) !== undefined) {
+                if (sessionStorage.narutocount || sessionStorage.sasukecount || sessionStorage.totalcount) {
+                    sessionStorage.narutocount = Number(sessionStorage.narutocount);
+                    sessionStorage.sasukecount = Number(sessionStorage.sasukecount);
+                    sessionStorage.totalcount = Number(sessionStorage.totalcount);
+                } else {
+                    sessionStorage.narutocount = sessionStorage.narutocount;
+                    sessionStorage.sasukecount = sessionStorage.sasukecount;
+                    sessionStorage.totalcount = sessionStorage.totalcount;
+                }
+            }
+            if (event.target == document.getElementById("naruto_id")){
+                sessionStorage.narutocount++;
+            } else if (event.target == document.getElementById("sasuke_id")){
+                sessionStorage.sasukecount++;
+            }
+            sessionStorage.totalcount = sessionStorage.narutocount + sessionStorage.sasukecount;
+            var porcentagemNaruto, porcentagemSasuke;
+            porcentagemNaruto = Math.round((sessionStorage.narutocount / sessionStorage.totalcount) * 100);
+            porcentagemSasuke = Math.round((sessionStorage.sasukecount / sessionStorage.totalcount) * 100); 
+            console.log('Naruto votos = ' + porcentagemNaruto);
+            console.log('Sasuke votos = ' + porcentagemSasuke);
+            votacao.style.display = "none";
+            var narutoBarra = document.getElementById("narutoBarra_id");
+            narutoBarra.style.width = porcentagemNaruto + "%";
+            narutoBarra.innerHTML = porcentagemNaruto + "%";
+            document.getElementsByClassName("container-voto")[0].style.display = "block";   
+    };
+    //  sessionStorage.clear();    
 }
