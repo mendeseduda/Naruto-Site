75db659468dcd0389ba1c23e98b95fa74b62aa1f
vicious721 <du-du7@hotmail.com>
2018-06-03 13:55:30 -0300

    Criando votação
    Me tornando o Deus do js

--- JS/responsive.js
+++ JS/responsive.js
@@ -22,35 +22,41 @@ function closeNav() {
 
 
 window.onload = function(){ 
+    //sessionStorage.clear();
     var votacao = document.getElementById("votacao_id");
+    var votosNaruto = 0, votosSasuke = 0, totalVotos = 0;
     votacao.onclick = function(event) {
-            if (typeof(Storage) !== undefined) {
-                if (sessionStorage.narutocount || sessionStorage.sasukecount || sessionStorage.totalcount) {
-                    sessionStorage.narutocount = Number(sessionStorage.narutocount);
-                    sessionStorage.sasukecount = Number(sessionStorage.sasukecount);
-                    sessionStorage.totalcount = Number(sessionStorage.totalcount);
-                } else {
-                    sessionStorage.narutocount = sessionStorage.narutocount;
-                    sessionStorage.sasukecount = sessionStorage.sasukecount;
-                    sessionStorage.totalcount = sessionStorage.totalcount;
-                }
+            if (sessionStorage.getItem("narutoVotos") === null || sessionStorage.getItem("sasukeVotos") === null
+            || sessionStorage.getItem("totalVotos") === null){
+                sessionStorage.setItem("narutoVotos", 0);
+                sessionStorage.setItem("sasukeVotos", 0);
+                sessionStorage.setItem("totalVotos", 0);
+            } else {
+                votosNaruto = Number(sessionStorage.getItem("narutoVotos"));
+                votosSasuke = Number(sessionStorage.getItem("sasukeVotos"));
+                totalVotos = Number(sessionStorage.getItem("totalVotos"));
             }
+            console.log(votosNaruto);
+            console.log(votosSasuke);
+            console.log(totalVotos);
             if (event.target == document.getElementById("naruto_id")){
-                sessionStorage.narutocount++;
+                votosNaruto++;
             } else if (event.target == document.getElementById("sasuke_id")){
-                sessionStorage.sasukecount++;
+                votosSasuke++;
             }
-            sessionStorage.totalcount = sessionStorage.narutocount + sessionStorage.sasukecount;
+            totalVotos = votosNaruto + votosSasuke;
             var porcentagemNaruto, porcentagemSasuke;
-            porcentagemNaruto = Math.round((sessionStorage.narutocount / sessionStorage.totalcount) * 100);
-            porcentagemSasuke = Math.round((sessionStorage.sasukecount / sessionStorage.totalcount) * 100); 
+            porcentagemNaruto = Math.round((votosNaruto / totalVotos) * 100);
+            porcentagemSasuke = Math.round((votosSasuke / totalVotos) * 100); 
             console.log('Naruto votos = ' + porcentagemNaruto);
             console.log('Sasuke votos = ' + porcentagemSasuke);
             votacao.style.display = "none";
             var narutoBarra = document.getElementById("narutoBarra_id");
             narutoBarra.style.width = porcentagemNaruto + "%";
             narutoBarra.innerHTML = porcentagemNaruto + "%";
-            document.getElementsByClassName("container-voto")[0].style.display = "block";   
-    };
-    //  sessionStorage.clear();    
+            document.getElementsByClassName("container-voto")[0].style.display = "block";  
+            sessionStorage.setItem("narutoVotos", votosNaruto);
+            sessionStorage.setItem("sasukeVotos", votosSasuke);
+            sessionStorage.setItem("totalVotos", totalVotos);
+    };    
 }
