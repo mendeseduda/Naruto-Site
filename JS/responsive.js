8672f69f2103f1b842ce1e1af38cb68b19e8f7c7
vicious721 <du-du7@hotmail.com>
2018-06-13 23:50:39 -0300

    Responsividade na página bijuus

--- JS/responsive.js
+++ JS/responsive.js
@@ -1,21 +1,8 @@
-var aberto = false;
-
-window.onclick = function(event) {
-    if (event.target == document.getElementById("Sidenav_id")) {
-        closeNav();
-    }
-}
-
 function openNav() {
-    if (aberto === false){
         document.getElementById("Sidenav_id").style.width = "250px";
-        aberto = true;
-    } else {
-        closeNav();
-    }
 }
 
 function closeNav() {
     document.getElementById("Sidenav_id").style.width = "0";
-    aberto = false;
+
 }
