1e578d47522afa6e8fd60243e807e86828977024
vicious721 <du-du7@hotmail.com>
2018-06-14 18:08:41 -0300

    Delete teste.js

--- JS/teste.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".teste").mouseenter(function () {
-        $(this).css('box-shadow', '0 0 2px #888');
-    });
-    $(".teste").mouseleave(function () {
-        $(this).css('box-shadow', 'none');
-    });
-})
