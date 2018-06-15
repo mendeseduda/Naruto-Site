02aa839e59da7bb5563064df535fac4951320682
Eduardo <du-du7@hotmail.com>
2018-06-15 18:07:49 -0300

    Modificações página bijuus
    Adição de botões para os bijuus

---
+++ JS/bijuusPage.js
@@ -0,0 +1,6 @@
+function shukakuClick(){
+    var descricao = document.getElementById("descricao_id");
+    var wrapper = document.getElementById("wrapper")
+    descricao.style.display = "flex";
+    wrapper.style.display = "none";
+}
