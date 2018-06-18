3b9ddf6456506f94b5823c28d5f30b416012512d
vicious721 <du-du7@hotmail.com>
2018-06-17 21:10:23 -0300

    Término do design da página akatsuki

---
+++ JS/akatsukiPage.js
@@ -0,0 +1,30 @@
+var akatsukiGroup = document.getElementById("akatsuki-group_id");
+var section = document.getElementById("sectionPricipal_id");
+var textoPrincipal = document.getElementById("textoCentral_id");
+var voltarBtn = document.getElementById("voltar_id");
+var h1 = document.getElementById("texto_id")
+
+voltarBtn.onclick = function(){
+    trocarTela();
+}
+
+function akatsukiClick(membro){
+    trocarTela(membro);
+}
+
+function trocarTela(texto){
+    if (akatsukiGroup.style.display !== "none"){
+        akatsukiGroup.style.display = "none";
+        textoPrincipal.style.transform = "none";
+        section.setAttribute('style', 'display: block !important');
+        textoPrincipal.style.position = "static";
+        textoPrincipal.style.border = "none";
+        h1.innerHTML = texto;
+    } else {
+        akatsukiGroup.style.display = "flex";
+        section.setAttribute('style', 'display: none !important');
+        textoPrincipal.style.position = "absolute";
+        textoPrincipal.style.transform = "translate(-50%, -50%)";
+        h1.innerHTML = "Akatsuki";
+    }
+}
