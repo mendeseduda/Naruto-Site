1b2fa5a49cc0cde19ed6a687c051d21354dad602
vicious721 <du-du7@hotmail.com>
2018-06-19 01:15:03 -0300

    Término página bijuus

--- JS/akatsukiPage.js
+++ JS/akatsukiPage.js
@@ -1,33 +1,13 @@
 var akatsukiGroup = document.getElementById("akatsuki-group_id");
-var section = document.getElementById("sectionPricipal_id");
-var textoPrincipal = document.getElementById("textoCentral_id");
-var voltarBtn = document.getElementById("voltar_id");
-var h1 = document.getElementById("texto_id")
+var textoPrincipal = document.getElementById("texto_id");
 var audio = document.getElementById("audioTheme");
 
-voltarBtn.onclick = function(){
-    trocarTela();
+function akatsukiOver(membro){
+    textoPrincipal.innerHTML = membro;
 }
 
-function akatsukiClick(membro){
-    trocarTela(membro);
-}
-
-function trocarTela(texto){
-    if (akatsukiGroup.style.display !== "none"){
-        akatsukiGroup.style.display = "none";
-        textoPrincipal.style.transform = "none";
-        section.setAttribute('style', 'display: block !important');
-        textoPrincipal.style.position = "static";
-        textoPrincipal.style.border = "none";
-        h1.innerHTML = texto;
-    } else {
-        akatsukiGroup.style.display = "flex";
-        section.setAttribute('style', 'display: none !important');
-        textoPrincipal.style.position = "absolute";
-        textoPrincipal.style.transform = "translate(-50%, -50%)";
-        h1.innerHTML = "Akatsuki";
-    }
+function akatsukiout(){
+    textoPrincipal.innerHTML = "Akatsuki";
 }
 
 function muteAudio(){
